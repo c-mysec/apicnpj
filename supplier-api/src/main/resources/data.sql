ca9de32336900cccbf57d397a88287bc019c8956
clovis <csyd@gft.com>
2024-12-10 16:46:56 -0300

    code-nv applied changes

--- supplier-api/src/main/resources/data.sql
+++ supplier-api/src/main/resources/data.sql
@@ -1,4 +1,3 @@
-INSERT INTO supplier (Nome, Cnpj, NomeContato, EmailContato, TelefoneContato) VALUES 
-('Supplier A', 12345678000190, 'Contact A', 'contactA@example.com', '123456789'),
-('Supplier B', 98765432000101, 'Contact B', 'contactB@example.com', '987654321'),
-('Supplier C', 11222333000111, 'Contact C', 'contactC@example.com', '456789123');
+UPDATE supplier 
+SET Cnpj = UPPER(REGEXP_REPLACE(Cnpj, '[^A-Z0-9]', '')) 
+WHERE Cnpj IS NOT NULL;
